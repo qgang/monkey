36bb3d32ef19c082e0bb6747bd62d2c12f34d3e1
gang.qin <gang.qin@dianping.com>
2015-12-01 19:18:53 +0800

    fight game two

--- src/main/java/FightGame/GameMain.java
+++ src/main/java/FightGame/GameMain.java
@@ -15,17 +15,25 @@ public class GameMain {
     public static Player fight(Player a, Player b) {
         while (true) {
             b.setBloodValue(b.getBloodValue() - a.getAtk()); // a 攻击 b
-            if (isDead(a)) {
-                return a;
-            }
-            a.setBloodValue(a.getBloodValue() - b.getAtk()); // b 攻击 a
+            printFight(a, b);
             if (isDead(b)) {
                 return b;
+            }
+            a.setBloodValue(a.getBloodValue() - b.getAtk()); // b 攻击 a
+            printFight(b, a);
+            if (isDead(a)) {
+                return a;
             }
         }
     }
 
     public static boolean isDead(Player p) {
         return p.getBloodValue() <= 0;
+    }
+
+    public static void printFight (Player attacker, Player victim) {
+        System.out.println(attacker.getName() + "攻击了" + victim.getName() + ","
+                         + victim.getName() + "受到了" + attacker.getAtk() + "点伤害" + ","
+                         + victim.getName() + "剩余生命：" + victim.getBloodValue());
     }
 }
