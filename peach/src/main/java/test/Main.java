01d001e75d87f525416dc050a6ad534b9b4521c9
gang.qin <gang.qin@dianping.com>
2016-09-30 16:33:19 +0800

    ikanalyzer

--- peach/src/main/java/test/Main.java
+++ peach/src/main/java/test/Main.java
@@ -6,24 +6,9 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
-        Calendar c = Calendar.getInstance();
-        c.set(Calendar.HOUR_OF_DAY, 0);
-        c.set(Calendar.MINUTE, 0);
-        c.set(Calendar.SECOND, 0);
-        Date start = c.getTime();
-
-        c.add(Calendar.DAY_OF_MONTH, 1);
-        Date end = c.getTime();
-        long diff = Math.abs(start.getTime() - end.getTime());
-        System.out.print("\nstart:" + sdf.format(start)
-                        +"\nend:" + sdf.format(end)
-                        +"\ndiff:" + diff
-                        +"\nmaxv:" + Long.MAX_VALUE
-                        +"\n" + (Long.MAX_VALUE > diff)
-                        +"\nmaxd:" + Long.MAX_VALUE/1000/60/60/24/365
-                        +"\n");
+        String str = "";
+        String[] list = str.split(",");
+        System.out.println(list);
     }
 }
 
