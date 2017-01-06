505637facef91c51b7a6d9b5b26815b952aadeb8
gang.qin <gang.qin@dianping.com>
2017-01-06 15:56:17 +0800

    fix

--- peach/src/main/java/io/DirList2.java
+++ peach/src/main/java/io/DirList2.java
@@ -1,11 +1,6 @@
 package io;
 
-import Util.FileUtil;
-
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FilenameFilter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
--- peach/src/main/java/test/IKAnalyzerTest.java
+++ peach/src/main/java/test/IKAnalyzerTest.java
@@ -1,25 +1,10 @@
 package test;
 
-import org.apache.lucene.analysis.TokenStream;
-import org.wltea.analyzer.lucene.IKAnalyzer;
-
-import java.io.FileReader;
-
 /**
  * Created by gang.qin on 2016/9/29.
  */
 public class IKAnalyzerTest {
     public static void main(String[] args) {
-        IKAnalyzer analyzer = new IKAnalyzer();
-        try {
-            TokenStream tokenStream = analyzer.tokenStream("content", new FileReader());
-            tokenStream.addAttribute(CharTermAttribute.class);
-            while (tokenStream.incrementToken()) {
-                CharTermAttribute charTermAttribute = tokenStream.getAttribute(CharTermAttribute.class);
-                System.out.println(charTermAttribute.toString());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
     }
 }
